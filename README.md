98f7059922da4542cc57d3f8a7b6b7bc5d66bd60
Laura Atencio <lauraatenciob@gmail.com>
2024-04-15 15:46:08 -0500

    links and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # async-landing
+
+This is a landing page about trending movies and is a project of the course of Asynchronism at Platzi. This application was released to production using Github Pages. You can see a preview in this [link](https://lauraatenciob.github.io/async-landing/src/).
--- src/assets/movieList.js
+++ src/assets/movieList.js
@@ -5,7 +5,7 @@
       ${movies
         .map(
           (movie) => `
-          <a href="/src/detail.html?id=${movie.id}">
+          <a href="detail.html?id=${movie.id}">
         <div class="group relative">
       <div
         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
